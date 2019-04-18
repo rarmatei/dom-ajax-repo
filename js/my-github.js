71fafc76fa92308b503711c24ae2c7ed90a6eda9
mohamedlafiteh <m_lafiteh@yahoo.com>
2019-04-18 14:05:51 +0100

    project github files

--- js/my-github.js
+++ js/my-github.js
@@ -1,2 +1,19 @@
 // Write code here to communicate with Github
+// var button = document.querySelector(".nav-link js-scroll-trigger");
+// button.addEventListener("click", function() {
 
+// })
+// button.textContent = ""
+var myUrl = "https://api.github.com/users/mohamedlafiteh/repos";
+// fetch(myUrl)
+// .then(function(response){
+// response.json()
+// })
+// .then(function(results){
+// results.forEach(function(name,id){
+//   var x = document.getElementById("repos-count");
+//   x.innerText = id.size;
+  
+// })
+// })
+fetch(myUrl).then(result => result.json()).then(repoList => repoList.forEach(repo => console.log(repo.full_name)));
